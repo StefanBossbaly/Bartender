cc6895fe3eb2cbeba9c8bdcb634092ca7bb234dd
Stefan Bossbaly <sbossb@gmail.com>
2014-02-17 22:57:41 -0500

    Added the "Add Beverage" Button
    Added the "Add Beverage" Button and code to save the beverage
    to the AddBeverageFragment. This will allow the user to save the
    beverage to the database. Still need to make the code more robus.

--- src/uofs/robotics/bartender/fragments/AddBeverageFragment.java
+++ src/uofs/robotics/bartender/fragments/AddBeverageFragment.java
@@ -1,22 +1,26 @@
 package uofs.robotics.bartender.fragments;
 
 import uofs.robotics.bartender.R;
-import android.app.Activity;
+import uofs.robotics.bartender.models.Beverage;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class AddBeverageFragment extends Fragment {
+public class AddBeverageFragment extends Fragment implements OnClickListener {
 
 	private EditText editName;
 	private Spinner spinnerType;
+	private Button buttonAdd;
 
-	public AddBeverageFragment() {
+	public AddBeverageFragment()
+	{
 
 	}
 
@@ -27,9 +31,12 @@ public class AddBeverageFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_add_beverage, container,
 				false);
 
+		// Get the XML objects
 		editName = (EditText) view.findViewById(R.id.edit_beverage_name);
 		spinnerType = (Spinner) view.findViewById(R.id.spinner_beverage_type);
+		buttonAdd = (Button) view.findViewById(R.id.button_add_beverage);
 		
+		// Set up the spinner
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 				getActivity(), R.array.beverage_types,
 				android.R.layout.simple_spinner_item);
@@ -37,12 +44,28 @@ public class AddBeverageFragment extends Fragment {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		
 		spinnerType.setAdapter(adapter);
+		
+		// Set up the button
+		buttonAdd.setOnClickListener(this);
 
 		return view;
 	}
 
 	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
+	public void onClick(View view) {
+		
+		if (view.getId() == R.id.button_add_beverage)
+		{
+			// Get the values out of the widgets
+			String name = editName.getText().toString();
+			String type = spinnerType.getSelectedItem().toString();
+			
+			// Create the beverage and save it
+			Beverage beverage = new Beverage(name, type);
+			beverage.save();
+			
+			// We are done here
+			getActivity().finish();
+		}
 	}
 }

2af2926b5fff87483295d8027773f0b4d9f2be09
Stefan Bossbaly <sbossb@gmail.com>
2014-02-26 19:43:26 -0500

    Added BeverageDrink Model
    This is the model that will link the beverage and drink models
    together so that the user can add beverages to their drinks. I
    still need to create the methods on Drink and Beverage that will
    get the ingridients.

---
+++ src/uofs/robotics/bartender/models/BeverageDrink.java
@@ -0,0 +1,53 @@
+package uofs.robotics.bartender.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "beverage_drink")
+public class BeverageDrink extends Model {
+
+	@Column(name = "drink")
+	private Drink drink;
+
+	@Column(name = "beverage")
+	private Beverage beverage;
+
+	@Column(name = "amount")
+	private double amount;
+
+	public BeverageDrink() {
+
+	}
+
+	public BeverageDrink(Beverage beverage, Drink drink, double amount) {
+		this.beverage = beverage;
+		this.drink = drink;
+		this.amount = amount;
+	}
+
+	public Drink getDrink() {
+		return drink;
+	}
+
+	public void setDrink(Drink drink) {
+		this.drink = drink;
+	}
+
+	public Beverage getBeverage() {
+		return beverage;
+	}
+
+	public void setBeverage(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+}
